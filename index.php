d9d2bf5eba9171f853439c6523e8c2b08d383277
pedrolazzaroni <phlazzaroni@gmail.com>
2025-05-11 10:57:22 -0300

    Add Space

--- index.php
+++ index.php
@@ -374,6 +374,7 @@
             </div>
         </section>
 
+
         <!-- Academic Section - NOVA SEÇÃO -->
         <section id="academic" class="academic-section section-padding">
             <div class="container">
