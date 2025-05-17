cfae6a4ad8ed708394101c7abfeb1d3b191e231e
pedrolazzaroni <phlazzaroni@gmail.com>
2025-05-17 11:55:09 -0300

    Ajustando Loading Screen

--- index.php
+++ index.php
@@ -109,9 +109,6 @@
     <!-- Loading Screen -->
     <div class="loading-screen">
         <div class="loader">
-            <svg viewBox="0 0 80 80">
-                <circle id="loader-circle" cx="40" cy="40" r="32"></circle>
-            </svg>
             <div class="loader-name">PEDRO LAZZARONI</div>
         </div>
     </div>
